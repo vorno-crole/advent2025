0922b3fcec8ae5a113789721379b71fbfbfeec02
Vaughan Crole <vc@vaughancrole.com>
2025-12-03 21:00:33 +1100

    day 3 pt2 - incomplete

---
+++ day3/day3-pt2.sh
@@ -0,0 +1,280 @@
+#!/usr/bin/env bash
+
+# setup
+	YLW="\033[33;1m"
+	GRN="\033[32;1m"
+	WHT="\033[97;1m"
+	BLU="\033[94;1m"
+	RED="\033[91;1m"
+	RES="\033[0m"
+
+	input_file="input.txt"
+
+	# functions
+		pause()
+		{
+			read -p "Press Enter to continue."
+		}
+	# end functions
+
+	if [[ $1 == 'example' ]]; then
+		input_file="example.txt"
+	fi
+# setup
+
+
+# functions
+	remove-last-chars()
+	{
+		local string=$1
+		local num=$2
+
+		# Calculate the new length
+		new_length=$(( ${#string} - num ))
+
+		# Extract the substring
+		result="${string:0:$new_length}"
+
+		echo "$result"
+	}
+
+	index-of()
+	{
+		local text=$1
+		local search=$2
+		# text="MULTI: primary virtual IP for xyz/x.x.x.x:44595: 10.0.0.12"
+		# search="IP for"
+
+		prefix=${text%%$search*}
+		echo ${#prefix}
+	}
+
+	test-number()
+	{
+		local num=$1
+		local numLen=${#num}
+		local tens
+		local units
+
+		# TODO
+		# find the largest 12 digit number, without rearranging the digits in $num
+		echo -ne "$line "
+		
+		local answer=""
+		local i
+		local j
+		local startChar=0
+
+		for (( i = 11; i >= 0; i-- )); do
+			echo -ne "$i "
+			for (( j = 9; j >= 1; j-- )); do
+				echo -ne "$j - "
+				substr1=$(remove-last-chars $num $i)
+				substr2=${substr1:$startChar}
+				echo -ne "$substr1 $substr2 "
+
+				if [[ $substr2 =~ $j ]]; then
+					answer+="$j"
+					startChar=$(index-of $substr1 $j)
+					echo -ne "startChar $startChar "
+					break;
+				fi
+			done;
+			echo
+		done;
+
+		echo $answer
+		exit;
+
+		return 1;
+	}
+
+	test-line()
+	{
+		local line=$1
+		local outputfile=$2
+		# echo -ne "$line : "
+
+		answer=$(test-number "$line")
+		echo "$line=$answer" >> ${outputfile}
+	}
+
+	check-answer()
+	{
+		local answer=$1
+		local expected=$2
+
+		if [[ $expected != "" ]]; then 
+			if [[ $answer -eq $expected ]]; then
+				echo "OK"
+			else
+				echo "Error"
+				exit 1
+			fi
+		else
+			echo
+		fi
+	}
+# functions
+
+output_file=output.txt
+rm -f ${output_file}
+
+# progress bar
+# total=$(wc -l $input_file | grep -Eo '\d+')
+# echo -ne "Progress:\n "
+
+# for (( j = 1; j <= total; j++ )); do
+# 	echo -ne " "
+# done
+# echo -ne "]\r["
+
+# read the input file
+sum=0
+
+while IFS='|' read -u 11 -r line expected; do
+	echo -ne "$line : "
+	lineLen=${#line}
+	outputLen=12
+	answer=""
+
+	# rm -f ${output_file};
+
+	# split the string by char. 
+	# output "index|value" to a temp file, for sorting
+	i=0
+	while read -u 12 digit; do
+		digits[$i]=$digit
+		# echo "$digit|$i" >> ${output_file}
+		((i++))
+	done 12< <(echo $line | grep -o .)
+
+	# echo; cat ${output_file};
+
+	# find the highest digit between index 0-> (15-12) (lineLen - outputLen)
+	highestValue=-1
+	highestValueIdx=-1
+	for (( i=0; i <= lineLen-outputLen ; i++ )); do
+		if [[ ${digits[$i]} -gt $highestValue ]]; then
+			highestValue=${digits[$i]};
+			highestValueIdx=$i;
+		fi
+	done
+
+	echo "[$highestValueIdx] $highestValue"
+	answer+="$highestValue"
+
+	# find the highest digit between index 0-> (15-12) (lineLen - outputLen)
+
+
+
+
+
+	exit;
+	echo $(())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	# sort the file, get the top 12 values, read them in.
+	declare -A digits=()
+	while IFS='|' read -u 12 digit idx; do
+		digits[$idx]=$digit
+	done 12< <(sort -nr ${output_file} | head -n 12)
+
+	while read -u 12 idx; do
+		answer+="${digits[$idx]}"
+	done 12< <(printf "%s\n" "${!digits[@]}" | sort -n)
+
+	echo "$answer"
+	# exit
+
+
+	# test-number "$line"
+	# # test-line "$line" "$output_file" && echo -ne "." &
+	break;
+done 11< $input_file
+
+exit;
+
+
+# Read/sum output file
+sum=$(awk -F'=' '{ sum += $2; } END { print sum }' ${output_file})
+echo -e "\n"
+echo "Sum             = $sum"
+
+# rm -f ${output_file}
+exit;
+
+
+# --- Part Two ---
+# The escalator doesn't move. 
+# The Elf explains that it probably needs more joltage to overcome the static friction of the system and hits the big red "joltage limit safety override" button. 
+# You lose count of the number of times she needs to confirm "yes, I'm sure" and decorate the lobby a bit while you wait.
+
+# Now, you need to make the largest joltage by turning on exactly twelve batteries within each bank.
+
+# The joltage output for the bank is still the number formed by the digits of the batteries you've turned on; 
+# the only difference is that now there will be 12 digits in each bank's joltage output instead of two.
+
+# Consider again the example from before:
+
+# 987654321111111
+# 811111111111119
+# 234234234234278
+# 818181911112111
+
+# Now, the joltages are much larger:
+
+# In 987654321111111, the largest joltage can be found by turning on everything except some 1s at the end to produce 987654321111.
+# In the digit sequence 811111111111119, the largest joltage can be found by turning on everything except some 1s, producing 811111111119.
+# In 234234234234278, the largest joltage can be found by turning on everything except a 2 battery, a 3 battery, and another 2 battery near the start to produce 434234234278.
+# In 818181911112111, the joltage 888911112111 is produced by turning on everything except some 1s near the front.
+# The total output joltage is now much larger: 987654321111 + 811111111119 + 434234234278 + 888911112111 = 3121910778619.
+
+
+# 987654321111111 : 987654321111
+# 811111111111119 : 811111111119
+# 234234234234278 : 343423423478
+# 818181911112111 : 881819111211
+
+# What is the new total output joltage?
+
+
+# --- Part One ---
+
+# There are batteries nearby that can supply emergency power to the escalator for just such an occasion. 
+# The batteries are each labeled with their joltage rating, a value from 1 to 9.
+# You make a note of their joltage ratings (your puzzle input). For example:
+
+# 987654321111111
+# 811111111111119
+# 234234234234278
+# 818181911112111
+# The batteries are arranged into banks; each line of digits in your input corresponds to a single bank of batteries. 
+# Within each bank, you need to turn on exactly two batteries; 
+# the joltage that the bank produces is equal to the number formed by the digits on the batteries you've turned on. 
+# For example, if you have a bank like 12345 and you turn on batteries 2 and 4, the bank would produce 24 jolts. (You cannot rearrange batteries.)
+
+# You'll need to find the largest possible joltage each bank can produce. In the above example:
+
+# In 987654321111111, you can make the largest joltage possible, 98, by turning on the first two batteries.
+# In 811111111111119, you can make the largest joltage possible by turning on the batteries labeled 8 and 9, producing 89 jolts.
+# In 234234234234278, you can make 78 by turning on the last two batteries (marked 7 and 8).
+# In 818181911112111, the largest joltage you can produce is 92.
+# The total output joltage is the sum of the maximum joltage from each bank, so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
+
+# There are many batteries in front of you. Find the maximum joltage possible from each bank; what is the total output joltage?
+
